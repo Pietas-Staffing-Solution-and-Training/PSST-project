c260477f1e556da0a0fd1d4072045ce323104bcb
41669436_DeJager <41669436@mynwu.ac.za>
2024-08-14 15:35:39 +0200

    Added sample gridview

--- PSST/PSST/Dashboard.aspx.designer.cs
+++ PSST/PSST/Dashboard.aspx.designer.cs
@@ -51,6 +51,15 @@ namespace PSST
         protected global::System.Web.UI.WebControls.Label lblWelcome;
 
         /// <summary>
+        /// ClientData control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView ClientData;
+
+        /// <summary>
         /// Image1 control.
         /// </summary>
         /// <remarks>
