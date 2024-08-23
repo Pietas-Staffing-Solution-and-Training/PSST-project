23c204df9331b2123d8a588fe914237f369cd5d0
Jurie-Hannes <jhblom2001@gmail.com>
2024-08-23 17:20:59 +0200

    Added Jobs Page with resource functionality for now :)

---
+++ PSST/PSST/Jobs.aspx.cs
@@ -0,0 +1,422 @@
+﻿using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using System.Drawing;
+using System.Data.SqlClient;
+using static QuestPDF.Helpers.Colors;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+
+namespace PSST
+{
+    public partial class Jobs : System.Web.UI.Page
+    {
+        MySqlConnection con;
+        string connectionString = ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            //tbUsername.Text = "Ruan@email.com";
+            //tbPassword.Text = "TestThisP@s5W0rD!";
+
+            //Get session value - returns null if doesn't exist
+            string username = Session["username"]?.ToString();
+            string type = Session["type"]?.ToString();
+            type = "admin"; // REMOVE IN PRODUCTION
+
+            //If string is null
+            if (username == null)
+            {
+                Response.Redirect("Login.aspx");
+                return;
+            }
+
+            if (type == "admin")
+            {
+
+            }
+            else
+            {
+                adminPanel.Visible = false;
+            }
+
+            if (!IsPostBack)
+            {
+                BindGridView();
+                FillIDBox();
+                divError.Visible = false;
+            }
+        }
+
+        protected void JobData_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedRow = JobData.SelectedIndex;
+            GridViewRow row = JobData.Rows[selectedRow];
+
+            int id = Convert.ToInt32(row.Cells[3].Text);
+
+            Session["JobId"] = id;
+            Response.Redirect("InvoiceForm.aspx");
+             //then in invoice Page_Load you do the following:
+             //string JobId = Session["JobID"] as string;
+        }
+
+        private void BindGridView()
+        {
+
+            string query = "SELECT Resource_ID, FName AS 'First Name', LName AS 'Last Name', Phone_Num AS 'Phone Number', ROUND(Wage, 2) AS 'Wage p/h', Competencies FROM RESOURCE";
+
+            try
+            {
+                using (con = new MySqlConnection(connectionString))
+                {
+                    con.Open();
+
+                    MySqlDataAdapter da = new MySqlDataAdapter(query, con);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    JobData.DataSource = dt;
+                    JobData.DataBind();
+
+                    con.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                showError(ex.Message);
+            }
+
+            // Bind the DataTable to the GridView
+
+        }
+
+        private void FillIDBox() // Gets the next ID
+        {
+            string query = "SELECT MAX(Resource_ID) FROM RESOURCE";
+            try
+            {
+                txtFName.Text = string.Empty;
+                txtLName.Text = string.Empty;
+                txtPhoneNum.Text = string.Empty;
+                txtWage.Text = string.Empty;
+                txtCompetencies.Text = string.Empty;
+
+                using (MySqlConnection con = new MySqlConnection(connectionString))
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, con);
+                    con.Open();
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            int highestID = reader.GetInt32(0);
+                            int nextID = highestID + 1;
+
+                            // Set the new ID in the TextBox
+                            txtID.Text = nextID.ToString();
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                showError(ex.Message);
+            }
+        }
+
+        protected void JobData_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int id = Convert.ToInt32(JobData.DataKeys[e.RowIndex].Value);
+
+            deleteRecord(id);
+
+            BindGridView();
+        }
+
+        protected void JobData_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            JobData.EditIndex = e.NewEditIndex;
+            BindGridView();
+
+            GridViewRow row = JobData.Rows[e.NewEditIndex];
+            TextBox tbName = (TextBox)row.Cells[4].Controls[0];
+            tbName.Focus();
+        }
+
+        protected void JobData_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                GridViewRow row = JobData.Rows[e.RowIndex];
+
+                int id = Convert.ToInt32(JobData.DataKeys[e.RowIndex].Value);
+                string name = ((TextBox)row.Cells[4].Controls[0]).Text;
+                string surname = ((TextBox)row.Cells[5].Controls[0]).Text;
+                string number = ((TextBox)row.Cells[6].Controls[0]).Text;
+                string wage = ((TextBox)row.Cells[7].Controls[0]).Text;
+                string competencies = ((TextBox)row.Cells[8].Controls[0]).Text;
+
+                updateRecord(id, name, surname, number, wage, competencies);
+
+                JobData.EditIndex = -1;
+                BindGridView();
+            }
+            catch (Exception ex)
+            {
+                showError(ex.Message);
+            }
+        }
+
+        protected void JobData_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            JobData.EditIndex = -1;
+            BindGridView();
+        }
+
+
+
+        protected void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            divError.Visible = false; // Hides errors when searching again
+            string search = txtSearch.Text;
+
+            string query = $"SELECT Resource_ID, FName AS 'First Name', LName AS 'Last Name', Phone_Num AS 'Phone Number', ROUND(Wage, 2) AS 'Wage p/h', Competencies FROM RESOURCE WHERE Resource_ID LIKE @SearchTerm OR FName LIKE @SearchTerm OR LName LIKE @SearchTerm OR Phone_Num LIKE @SearchTerm OR Wage LIKE @SearchTerm OR Competencies LIKE @SearchTerm";
+
+            try
+            {
+                using (con = new MySqlConnection(connectionString))
+                {
+
+                    MySqlCommand cmd = new MySqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@SearchTerm", "%" + search + "%");
+
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    if (dt.Rows.Count > 0)
+                    {
+                        JobData.DataSource = dt;
+                        JobData.DataBind();
+                    }
+                    else
+                    {
+                        // Show error message if no items are found
+                        showError($"No item found for {search}");
+                        JobData.DataSource = null;
+                        JobData.DataBind();
+                    };
+
+                }
+            }
+            catch (Exception ex)
+            {
+                showError(ex.Message);
+            }
+        }
+
+        protected void updateRecord(int id, string name, string surname, string number, string wage, string competencies)
+        {
+            string query = @"UPDATE RESOURCE SET FName = @FName, LName = @LName, Phone_Num = @PhoneNum, Wage = @Wage, Competencies = @Competencies WHERE Resource_ID = @ResourceID";
+
+            try
+            {
+                using (con = new MySqlConnection(connectionString))
+                {
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(query, con);
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@FName", name);
+                        cmd.Parameters.AddWithValue("@LName", surname);
+                        cmd.Parameters.AddWithValue("@PhoneNum", number);
+                        cmd.Parameters.AddWithValue("@Wage", wage);
+                        cmd.Parameters.AddWithValue("@Competencies", competencies);
+                        cmd.Parameters.AddWithValue("@ResourceID", id);
+
+                        con.Open();
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+
+                        adapter.UpdateCommand = cmd;
+                        adapter.UpdateCommand.ExecuteNonQuery();
+
+                        con.Close();
+
+                        BindGridView();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                showError(ex.Message);
+            }
+        }
+
+        protected void deleteRecord(int id)
+        {
+
+            string query = @"DELETE FROM RESOURCE WHERE Resource_ID = @ResourceID";
+
+            try
+            {
+                using (con = new MySqlConnection(connectionString))
+                {
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(query, con);
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@ResourceID", id);
+
+                        con.Open();
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+
+                        adapter.DeleteCommand = cmd;
+                        adapter.DeleteCommand.ExecuteNonQuery();
+
+                        con.Close();
+
+                        BindGridView();
+                    }
+                }
+                clearError();
+                FillIDBox();
+            }
+            catch (MySqlException)
+            {
+                string jobId = ifRhasJob(id);
+                showError($"Cannot delete resource because they are currently assigned a Job (ID: {jobId})");
+
+            }
+        }
+
+        protected string ifRhasJob(int id)
+        {
+            // Get the id of the job a resource is connected (If Resource Has Job = ifRhasJob)
+            string jobId = "";
+            string jobQuery = @"SELECT Job_ID FROM JOB WHERE Resource_ID = @Resource_ID";
+
+            using (con = new MySqlConnection(connectionString))
+            {
+
+                MySqlCommand command = new MySqlCommand(jobQuery, con);
+                command.Parameters.AddWithValue("@Resource_ID", id);
+
+                con.Open();
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+
+                    while (reader.Read())
+                    {
+                        jobId = reader["Job_ID"].ToString();
+                    }
+                }
+
+                con.Close();
+            }
+            return jobId;
+        }
+
+        protected void showError(string error)
+        {
+            divError.Visible = true;
+            lblError.Text = error;
+        }
+
+        protected void clearError()
+        {
+            divError.Visible = false;
+        }
+
+        protected void btnExitErr_Click(object sender, EventArgs e)
+        {
+            divError.Visible = false;
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e) // Acts as if text changed in textbox for search
+        {
+            txtSearch_TextChanged(sender, e);
+        }
+
+        protected void btnSearchClear_Click(object sender, EventArgs e)
+        {
+            txtSearch.Text = "";
+            txtSearch_TextChanged(sender, e);
+        }
+
+        protected void btnAddDB_Click(object sender, EventArgs e)
+        {
+            string query = @"INSERT INTO RESOURCE (Resource_ID, FName, LName, Phone_Num, Wage, Competencies) 
+                 VALUES (@ResourceID, @FName, @LName, @PhoneNum, @Wage, @Competencies)";
+
+            try
+            {
+                using (MySqlConnection con = new MySqlConnection(connectionString))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand(query, con))
+                    {
+                        int resourceID;
+                        string fName = txtFName.Text;
+                        string lName = txtLName.Text;
+                        string phoneNum = txtPhoneNum.Text;
+                        decimal wage;
+                        string competencies = txtCompetencies.Text;
+
+                        if (!(int.TryParse(txtID.Text, out resourceID)))
+                        {
+                            throw new Exception("Invalid Resource ID.");
+                        }
+
+                        if (!(Regex.IsMatch(phoneNum, @"^(\+27|0)[6-8][0-9]{8}$")))
+                        {
+                            throw new Exception("Invalid Phone Number.");
+                        }
+
+                        if (!(decimal.TryParse(txtWage.Text, out wage)))
+                        {
+                            throw new Exception("Invalid Wage.");
+                        }
+
+                        cmd.Parameters.AddWithValue("@ResourceID", txtID.Text);
+                        cmd.Parameters.AddWithValue("@FName", fName);
+                        cmd.Parameters.AddWithValue("@LName", lName);
+                        cmd.Parameters.AddWithValue("@PhoneNum", phoneNum);
+                        cmd.Parameters.AddWithValue("@Wage", wage);
+                        cmd.Parameters.AddWithValue("@Competencies", competencies);
+
+                        con.Open();
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        // Check if the insert was successful
+                        if (rowsAffected > 0)
+                        {
+                            BindGridView();
+                        }
+                        else
+                        {
+                            showError("Insert operation failed."); // Insert failed or no rows affected
+                        }
+
+                        con.Close();
+                    }
+                }
+
+                FillIDBox();
+                clearError();
+            }
+            catch (Exception ex)
+            {
+                showError(ex.Message);
+            }
+        }
+    }
+}
